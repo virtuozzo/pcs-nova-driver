3fb082135266d95f79480fe9d7280ba86d69d8d9
Dmitry Guryanov <dguryanov@parallels.com>
2013-12-06 21:52:32 +0400

    add script for creating glance images from rpms
    It's very unconvenient to execute glance command with a lot
    of arguments to upload ez-template to glance. So add helper script,
    which will do all that work.
    
    Usage:
    ./utils/install-image.py <rpm file>

---
+++ utils/install-image.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import commands
+import subprocess
+
+if len(sys.argv) != 2:
+    print "Usage: install-image.py <image-file> <extra glance args>"
+    sys.exit(1)
+
+if not os.path.exists(sys.argv[1]):
+    print "File '%s' is not found" % sys.argv[1]
+
+cmd = "rpm -q --qf '%%{NAME},%%{VERSION},%%{RELEASE}' -p '%s'" % sys.argv[1]
+ret, out = commands.getstatusoutput(cmd)
+if ret:
+    print "rpm returned %d" % ret
+
+name, ver, rel = out.split(',')
+image_name = name[:-3]
+
+cmd = ['glance', 'image-create',
+        '--name', image_name,
+        '--file', sys.argv[1],
+        '--property', 'pcs_name=%s' % name,
+        '--property', 'pcs_version=%s' % ver,
+        '--property', 'pcs_release=%s' % rel,
+        '--disk-format', 'raw',
+        '--container-format', 'bare']
+
+cmd += sys.argv[2:]
+
+p = subprocess.Popen(cmd)
+ret = p.wait()
+sys.exit(p.wait())
