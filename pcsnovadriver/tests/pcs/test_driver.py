561243a11dcb43935b1550cc0323b96ca4d2003f
Dmitry Guryanov <dguryanov@parallels.com>
2014-03-05 19:43:50 +0400

    add simple testcases for Driver.plug_vifs

--- pcsnovadriver/tests/pcs/test_driver.py
+++ pcsnovadriver/tests/pcs/test_driver.py
@@ -13,9 +13,12 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+import mock
+
 from oslo.config import cfg
 
 from nova.compute import power_state
+from nova.openstack.common import uuidutils
 from nova import test
 from nova.virt import fake
 
@@ -36,23 +39,43 @@ CONF.import_opt('instances_path', 'nova.compute.manager')
 
 CONF.firewall_driver = "nova.virt.firewall.NoopFirewallDriver"
 
-vms = [
-        {
+vm_stopped = {
             'name': 'instance001',
             'uuid': '{19be06cb-a6f2-47a7-a53e-11bc6d4c3b98}',
             'ram_size': 1024,
             'cpu_count': 1,
             'state': fakeprlsdkapi.consts.VMS_STOPPED,
-        },
-        {
+        }
+
+vm_running = {
             'name': 'instance002',
             'uuid': '{d58fe074-ce99-46b7-8ce1-83620ba26426}',
             'ram_size': 2048,
             'cpu_count': 4,
             'state': fakeprlsdkapi.consts.VMS_RUNNING,
-        },
-]
+        }
 
+vms = [vm_stopped, vm_running]
+
+instance_stopped = {
+            'name': vm_stopped['name'],
+            'power_state': power_state.SHUTDOWN
+        }
+
+instance_running = {
+            'name': vm_running['name'],
+            'power_state': power_state.RUNNING
+        }
+
+network_info_1vif = [
+                {'id': uuidutils.generate_uuid()},
+            ]
+
+network_info_3vif = [
+                {'id': uuidutils.generate_uuid()},
+                {'id': uuidutils.generate_uuid()},
+                {'id': uuidutils.generate_uuid()},
+            ]
 
 class PCSDriverTestCase(test.TestCase):
 
@@ -88,3 +111,41 @@ class PCSDriverTestCase(test.TestCase):
         self.assertEqual(info['num_cpu'], vm['cpu_count'])
         self.assertEqual(info['cpu_time'], 1000)
 
+    def _prep_plug_vifs(self, vm):
+        self.conn._get_ve_by_name = mock.MagicMock()
+        sdk_ve = fakeprlsdkapi.Vm(vm)
+        self.conn._get_ve_by_name.return_value = sdk_ve
+
+        self.conn.vif_driver = mock.MagicMock()
+
+        return sdk_ve
+
+    def test_plug_vifs_running(self):
+        vm = vm_running
+        sdk_ve = self._prep_plug_vifs(vm)
+
+        self.conn.plug_vifs(instance_running, network_info_1vif)
+
+        args = (self.conn, instance_running, sdk_ve, network_info_1vif[0],)
+        self.conn.vif_driver.plug.assert_called_once_with(*args)
+        self.assertEqual(sdk_ve.state, fakeprlsdkapi.consts.VMS_RUNNING)
+
+    def test_plug_vifs_stopped(self):
+        vm = vm_stopped
+        sdk_ve = self._prep_plug_vifs(vm)
+
+        self.conn.plug_vifs(instance_stopped, network_info_1vif)
+
+        self.assertEqual(self.conn.vif_driver.plug.call_count, 0)
+        self.assertEqual(sdk_ve.state, fakeprlsdkapi.consts.VMS_STOPPED)
+
+    def test_plug_vifs_unexistent(self):
+        instance = {
+                    'name': 'this_vm_does_not_exist',
+                    'power_state': power_state.RUNNING
+                }
+
+        self.conn.vif_driver = mock.MagicMock()
+        self.conn.plug_vifs(instance, network_info_1vif)
+
+        self.assertEqual(self.conn.vif_driver.plug.call_count, 0)
